1cec44ccf4796ee1c0be9572595d34fcce39aad9
nandhananandha2724-sys <nandhananandha2724@gmail.com>
2026-07-25 12:31:25 +0530

    category crud functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -34,6 +34,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    #Dashboards
+    path('dashboards/',include('dashboards.urls')),
 
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
